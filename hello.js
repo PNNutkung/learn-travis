24850208ad64e9421242328159bf71b508338c0c
PNNutkung <pipatpol.t@ku.th>
2017-04-05 18:35:58 +0700

    change arrow expression to function

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 const http = require('http');
-http.createServer((req, res) => {
+http.createServer(funtion (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
 }).listen(1337, '127.0.0.1');
